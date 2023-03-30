9df335eef6c82b67540e63252a92a00eca0f674d
ulol <jeffk0408@naver.com>
2023-03-31 02:30:00 +0900

    feat : FGCOBject를 이용한 일반 C++ì+오브젝트의 메모리 관리 방법

--- Source/UnrealMemory/StudentManager.h
+++ Source/UnrealMemory/StudentManager.h
@@ -3,16 +3,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
-
+#include "Student.h"
 /**
  * 
  */
-class UNREALMEMORY_API FStudentManager
+class UNREALMEMORY_API FStudentManager : public FGCObject
 {
 public:
 	FStudentManager(class UStudent* InStudent)
 		: SafeStudent(InStudent) {};
 	const class UStudent* GetStudent() const {return SafeStudent;}
+
+	virtual void AddReferencedObjects(FReferenceCollector& Collector) override
+	{
+		if (SafeStudent->IsValidLowLevel() == true)
+		{
+			// 등록하는 부분
+			Collector.AddReferencedObject(SafeStudent);
+			
+		}
+	}
+
+	virtual FString GetReferencerName() const override
+	{
+		// 고유한 클래스 이름을 반환해주면 된다.
+		return TEXT("FStudentManager");
+	}
 private:
 	class UStudent* SafeStudent = nullptr;
 };
